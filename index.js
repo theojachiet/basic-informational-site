8d4a9a9ec0d0086ed0ee475b2197063a39009825
Teoslip <tjachiet@gmail.com>
2026-02-28 16:36:54 +0100

    redirect to page according to route

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer(function (req, res) {
-    console.log(req)
-}).listen(3000);
+    //console.log(req.url)
+
+    if (req.url == '/') {
+        res.setHeader('Content-Type', 'text/html');
+
+        fs.readFile('./index.html', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.write(data);
+                res.end();
+            }
+        })
+    } else if (req.url == '/about') {
+        res.setHeader('Content-Type', 'text/html');
+
+        fs.readFile('./about.html', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.write(data);
+                res.end();
+            }
+        })
+    } else if (req.url == '/contact-me') {
+        res.setHeader('Content-Type', 'text/html');
+
+        fs.readFile('./contact-me.html', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.write(data);
+                res.end();
+            }
+        })
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+
+        fs.readFile('./404.html', (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.write(data);
+                res.end();
+            }
+        })
+    }
+});
 
 server.listen(3000, 'localhost', () => {
     console.log('listening on port 3000');
