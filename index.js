6d97c25353cb0a886b889c6e455d83108828b74b
Theo <tjachiet@gmail.com>
2026-03-05 20:47:52 +0100

    rewrite app with express

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(function (req, res) {
+app.listen(3000);
 
-    res.setHeader('Content-Type', 'text/html');
-
-    let path = './';
-
-    switch (req.url) {
-        case '/':
-            path += 'index.html'
-            break;
-        case '/about':
-            path += 'about.html'
-            break;
-        case '/contact-me':
-            path += 'contact-me.html'
-            break;
-        default:
-            path += '404.html'
-            break;
-    }
-
-    fs.readFile(path, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.end()
-        } else {
-            res.end(data);
-        }
-    })
-
-});
-
-server.listen(3000, 'localhost', () => {
-    console.log('listening on port 3000');
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', {root: __dirname});
 })
 
+app.get('/about', (req, res) => {
+    res.sendFile('./about.html', {root: __dirname});
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('./contact-me.html', {root: __dirname});
+})
+
+
+app.use((req, res) => {
+    res.status(404).sendFile('./404.html', {root: __dirname});
+})
